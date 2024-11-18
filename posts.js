64fb27f08e55c06b09b9b3e74ad3c7a196b051a7
AndreaPotenza <andreapoten3@gmail.com>
2024-11-18 16:15:52 +0100

    quasi completo

--- posts.js
+++ posts.js
@@ -1,4 +1,34 @@
 const postsList = [
+    {
+        titolo: "Ciambellone",
+        contenuto: "Lorem Ipsum",
+        img: "/img/ciambellone.jpeg",
+        tags:['']
+    },
+    {
+        titolo: "Cracker alla Barbabietola",
+        contenuto: "Lorem Ipsum",
+        img: "/img/cracker_barbabietola.jpeg",
+        tags:['']
+    },
+    {
+        titolo: "Pane fritto dolce",
+        contenuto: "Lorem Ipsum",
+        img: "/img/pane_fritto_dolce.jpeg",
+        tags:['']
+    },
+    {
+        titolo: "Pasta alla Barbabietola",
+        contenuto: "Lorem Ipsum",
+        img: "/img/pasta_barbabietola.jpeg",
+        tags:['']
+    },
+    {
+        titolo: "Torta paesana",
+        contenuto: "Lorem Ipsum",
+        img: "/img/torta_paesana.jpeg",
+        tags:['']
+    }
    
 ]
 
